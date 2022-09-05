11e863f6509bcb489d7d30afd3febc2f8bc4e737
aeyeon96 <dldodus6487@gmail.com>
2022-09-05 17:56:17 +0900

    [a]

--- FANTASTIC4_Question.java
+++ FANTASTIC4_Question.java
@@ -1,6 +1,18 @@
+import MiniProjectConnectMySQL;
+
 public class FANTASTIC4_Question {
+    
+    String[] output = {};
     public int questFunction(){
-        System.out.println("설문");
+
+        
+        // Store the output
+        String[] output = {};
+        System.out.println("--------설문을 시작합니다---------");
+        
+
+
+
         return 1;
     }
 }
