e97f03ae0e94f744f7cf0de78a25b3fafff1f593
shinwoejin <shinwoejin@naver.com>
2022-09-05 16:25:41 +0900

    commit

---
+++ FANTASTIC4_MAIN.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FANTASTIC4_MAIN{
+
+    public static void main(String[] args){
+        boolean run = true;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(run){
+            System.out.println("----------------------------------");
+            System.out.println(" P.설문시작 | S.통계 | Q.종료 ");
+            System.out.println("-------------------------------------");
+            System.out.print("선택) ");
+
+        }
+    }
+}
