aa1872e3e476bd2b074ea1082001eda5029998a3
shinwoejin <shinwoejin@naver.com>
2022-09-05 17:11:37 +0900

    commit

---
+++ FANTASTIC4_Question.java
@@ -0,0 +1,6 @@
+public class FANTASTIC4_Question {
+    public int questFunction(){
+        System.out.println("설문");
+        return 1;
+    }
+}
---
+++ FANTASTIC4_Static.java
@@ -0,0 +1,6 @@
+public class FANTASTIC4_Static {
+    public int statFunction(){
+        System.out.println("통계");
+        return 2;
+    }
+}
