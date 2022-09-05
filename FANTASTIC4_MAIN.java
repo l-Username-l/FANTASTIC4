11e6adebac5a99e908e92be08abbef314abb37a6
shinwoejin <shinwoejin@naver.com>
2022-09-05 16:43:07 +0900

    commit

--- FANTASTIC4_MAIN.java
+++ FANTASTIC4_MAIN.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-public class FANTASTIC4_MAIN{
+public class FANTASTIC4_MAIN {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         boolean run = true;
 
         Scanner scanner = new Scanner(System.in);
 
-        while(run){
+        while (run) {
             System.out.println("----------------------------------");
             System.out.println(" P.설문시작 | S.통계 | Q.종료 ");
             System.out.println("-------------------------------------");
             System.out.print("선택) ");
 
+            int selectNo = Integer.parseInt(scanner.nextLine());
+            
         }
     }
 }
