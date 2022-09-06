5d0f2e0b7e1738e0edfe17134861ec3f913786e6
aeyeon96 <dldodus6487@gmail.com>
2022-09-06 16:20:13 +0900

    a

--- src/FANTASTIC4_Question.java
+++ src/FANTASTIC4_Question.java
@@ -42,24 +42,25 @@ public class FANTASTIC4_Question {
             //stmt = FANTASTIC_MySQL_Connect.SQL_connect();
             rsQues = stmt.executeQuery(QUERY1);
             rsAns = stmt.executeQuery(QUERY2);
-            rsName = stmt.executeQuery(QUERY3);
+            //rsName = stmt.executeQuery(QUERY3);
 
             // Step3: 'name_no' table에 사용자 번호와 이름 update
             int usr_number;//Static 
             QUERY3 = "select count(*) from NAME_NO";
             ResultSet re_arbi = stmt.executeQuery(QUERY3);// + 1;
+            re_arbi.next();
             usr_number = re_arbi.getInt(1);
             usr_number += 1;
             // Update!!
-            QUERY3 = "insert into name_no (NAME_NO, NAME) values ("+usr_number+","+current_name+")";
+            QUERY3 = "insert into name_no (NAME_NO, NAME) values ("+usr_number+",\""+current_name+"\")";
             val = stmt.executeUpdate(QUERY3);
 
 
 
             // Step4: 'surv_no' table에서 질문 사항 불러들이기
             // Step5: 질문 사항 하위에 'ans_no' table에서 질문들 불러들이기
-            int i = 0;   // 여기서 오류남 
-            while (rsQues.next()) {
+            int i = 0;  // 여기서 오류남 
+            while (!rsQues.next()) { 
                 // Retrieve by column name
                 System.out.print(rsQues.getInt("SURV_NO")+"번 문항 : ");
                 System.out.println(rsQues.getString("SURV"));
@@ -74,11 +75,12 @@ public class FANTASTIC4_Question {
 
                 // Step6: 설문자의 답 받아들이기
                 System.out.print("--------답을 선택해 주세요 : ");
+
                 String choose_num = scanner.nextLine();
                 if (choose_num.length() == 1){
 
                     // Input the result of the question
-                    usr_output[i] = (String) choose_num;
+                    usr_output[i] = choose_num;
                     i++;
                 } else {
                     System.out.println("복수의 답이 입력 되었거나 답항지에 없는 번호입니다.");
