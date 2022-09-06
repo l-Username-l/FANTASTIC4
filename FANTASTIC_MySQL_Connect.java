69bc9c1d2dbc57f9427bddcca479cb391cdd2870
aeyeon96 <dldodus6487@gmail.com>
2022-09-06 15:27:23 +0900

    km

--- FANTASTIC_MySQL_Connect.java
+++ FANTASTIC_MySQL_Connect.java
@@ -5,28 +5,27 @@ import java.sql.Statement;
 
 public class FANTASTIC_MySQL_Connect {
 
-   
    Statement stmt;
-
    public Statement SQL_connect() {
-      
-      final String DB_URL = "jdbc:mysql://localhost/fantastic4";
+
+      // public Statement SQL_connect() {
+
+      final String DB_URL = "jdbc:mysql://localhost/FANTASTIC4";
       final String USER = "root";
       final String PASS = "tbrs00002b";
-      
+      //Statement stmt;
+      // Statement stmt;
+
       // Open a connection
       try {
          Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-         Statement stmt = conn.createStatement();
-         // return stmt;
+         stmt = conn.createStatement();
 
       } catch (SQLException e) {
          e.printStackTrace();
       }
-      return stmt;
-
+      return stmt ; // stmt;
    }
 
-   // FANTASTIC_MySQL_Connect con_arbi = new FANTASTIC_MySQL_Connect();
-   // return con_arbi.stmt;
+  
 }
