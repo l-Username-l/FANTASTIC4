4a8884534044a4d653ccfee01d7c6a2f739f4f27
shinwoejin <shinwoejin@naver.com>
2022-09-06 15:08:25 +0900

    commit

--- FANTASTIC_MySQL_Connect.java
+++ FANTASTIC_MySQL_Connect.java
@@ -3,27 +3,30 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class FANTASTIC_MySQL_Connect {
 
-   Statement stmt;
+   public static void main(String[] args) {
 
-   public Statement SQL_connect() {
-      
-   final String DB_URL = "jdbc:mysql://localhost/FANTASTIC4";
-   final String USER = "root";
-   final String PASS = "tbrs00002b";
-   
+      // public Statement SQL_connect() {
+
+      final String DB_URL = "jdbc:mysql://localhost/FANTASTIC4";
+      final String USER = "root";
+      final String PASS = "tbrs00002b";
+      // Statement stmt;
+
       // Open a connection
       try {
-         
          Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-         stmt = conn.createStatement();
+         Statement stmt = conn.createStatement();
          // return stmt;
 
       } catch (SQLException e) {
          e.printStackTrace();
       }
-      return stmt;
+      return; // stmt;
+   }
+
+   public Statement SQL_connect() {
+      return null;
    }
 }
